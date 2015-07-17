e37215dba07a4b1a5793b11fc8b8093ffe254299
R1tschY <r1tschy@yahoo.de>
2015-07-17 21:16:55 +0200

    better autogen.sh and configure.ac

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-autoreconf --install
+SRCDIR=`dirname $0`
+if [ -z "$SRCDIR" ]; then
+  SRCDIR=.
+fi
+
+aclocal --install || exit 1
+autoreconf --force --install || exit 1
+
+if [ "$NOCONFIGURE" = "" ]; then
+  $SRCDIR/configure "$@" || exit 1
+
+  if [ "$1" = "--help" ]; then 
+    exit 0 
+  else
+    echo "Now type \`make' to compile"
+  fi
+fi
